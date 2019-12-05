7fa1b16a4e32764a68aeaa4d98d86bab4c7e003f
stpnvsf <stpnvsfj@gmail.com>
2019-12-05 11:18:38 +0400

    ---

---
+++ WinApp/WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,78 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private Tao.Platform.Windows.SimpleOpenGlControl AnT;
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+       
+        private void InitializeComponent()
+         {
+            this.AnT = new Tao.Platform.Windows.SimpleOpenGlControl();
+            this.SuspendLayout();
+            // 
+            // AnT
+            // 
+            this.AnT.AccumBits = ((byte)(0));
+            this.AnT.AutoCheckErrors = false;
+            this.AnT.AutoFinish = false;
+            this.AnT.AutoMakeCurrent = true;
+            this.AnT.AutoSwapBuffers = true;
+            this.AnT.BackColor = System.Drawing.Color.Black;
+            this.AnT.ColorBits = ((byte)(32));
+            this.AnT.DepthBits = ((byte)(16));
+            this.AnT.Location = new System.Drawing.Point(32, 15);
+            this.AnT.Margin = new System.Windows.Forms.Padding(4);
+            this.AnT.Name = "AnT";
+            this.AnT.Size = new System.Drawing.Size(700, 700);
+            this.AnT.StencilBits = ((byte)(0));
+            this.AnT.TabIndex = 0;
+            this.AnT.Load += new System.EventHandler(this.simpleOpenGlControl1_Load);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(791, 738);
+            this.Controls.Add(this.AnT);
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+         }
+         
+        #endregion
+
+        /* private Tao.Platform.Windows.SimpleOpenGlControl AnT;
+         private System.Windows.Forms.Button button1;
+         private System.Windows.Forms.Button button2;
+         private System.Windows.Forms.TrackBar trackBar1;
+         private System.Windows.Forms.Timer timer1;*/
+    }
+}
+
---
+++ WinApp/WindowsFormsApp1/Form1.cs
@@ -0,0 +1,173 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Tao.OpenGl;
+using Tao.FreeGlut;
+using Tao.Platform.Windows;
+namespace WindowsFormsApp1
+{
+   
+    public partial class Form1 : Form
+    {
+        private SimpleOpenGlControl AnT;
+
+        //y' =  y - x^2 - исходное уравнение
+        //k = y - x^2
+        //y = x^2 + k, k = -4, .... 4 
+        private double y(double x, double k)
+        {
+            return Math.Pow(x, 2) + k;
+        }
+        //result расчитывает координаты точек интегральной кривой для заданных начальных условий (x0,y0)
+        private List<double> result(double x0, double y0)
+        {
+            List<double> coordinates = new List<double>();
+            double k;
+            double y, y1 = y0;
+            if (x0 < 4)
+            {
+                for (double i = x0; i <= 4; i += 0.25)
+                {
+                    k = y1 - Math.Pow(i, 2);//k = y1 - x^2 - найдем угловой коэффициент 
+                    y = i * k;//найдем y для наденного k
+                    coordinates.Add(y);//добавляем в список, хранящий координаты 
+                    y1 = y;//старому значению y1 присвоим новое значение у
+                }
+            }
+            else//в случае если x0 больше 4
+            {
+                //обратный ход 
+                for (double i = x0; i >= -4; i -= 0.25)
+                {
+                    k = y1 - Math.Pow(i, 2);
+                    y = (i - k) * k;
+                    coordinates.Add(y);
+                    y1 = y;
+                }
+            }
+            return coordinates;
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+            AnT.InitializeContexts();
+            print();       
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {            
+            Glut.glutInit();//инициализация,библиотека
+            Glut.glutInitDisplayMode(Glut.GLUT_RGBA | Glut.GLUT_DOUBLE);
+            Gl.glClearColor(0.0f, 0.0f, 0.0f, 1.0f);//цвет экрана
+            Gl.glViewport(0, 0, AnT.Width, AnT.Height);
+            Gl.glMatrixMode(Gl.GL_PROJECTION);//матрица проекции на экран
+            Gl.glLoadIdentity();
+            Glu.gluOrtho2D(0.0, AnT.Width, 0.0, AnT.Height);
+            
+            Gl.glMatrixMode(Gl.GL_MODELVIEW);
+            Gl.glLoadIdentity();
+            
+        }
+
+
+        public void print()// в методе print происходит отрисовка системы координат и решения уравнения
+        {
+            Gl.glColor3f(0.0f, 1.0f, 0.0f);
+            
+            //отрисовываем систему координат
+            Gl.glBegin(Gl.GL_LINES);
+            
+                Gl.glVertex2d(2.0, 0);
+                Gl.glVertex2d(-2.0, 0);
+                Gl.glVertex2d(0, 2.0);
+                Gl.glVertex2d(0, -2.0);
+
+            Gl.glEnd();
+            
+            //y = x^2 + k, k = -4, ....4 для каждого k рисуем график
+            Gl.glColor3f(1.0f, 0.0f, 1.0f);
+            for (double k = -4; k <= 4; k += 0.5)
+            {
+                Gl.glBegin(Gl.GL_LINE_STRIP);
+                for (double j = -4; j <= 4; j += 0.01)
+                {
+                    Gl.glVertex2d(j, y(j, k));// метод y(j,k) соответствует функции y = x^2 + k
+                }
+                Gl.glEnd();
+            }
+            //for (double k = -4; k <= 4; k += 0.5)
+            //{
+            //    Gl.glBegin(Gl.GL_LINES);
+            //    for (double j = -4; j <= 4; j += 0.3)
+            //    {
+            //        Gl.glVertex2d(j - 0.1, k * (j - 0.1) + y(j, k));
+            //        Gl.glVertex2d(j + 0.1, k * (j + 0.1) + y(j, k));
+
+            //    }
+            //    Gl.glEnd();
+            //}
+
+            // u, w  - задаем начальное условие
+            double u = -1;
+            double w = 1;
+            List<double> res = result(u, w);//получаем список координат точек интегральной кривой 
+                                            //для начального условия (u,w)
+            Gl.glColor3f(1.0f, 0.0f, 0.0f);
+            Gl.glBegin(Gl.GL_LINE_STRIP);
+            foreach(double r in res)
+            {
+                Gl.glVertex2d(u,r);//для каждго r из res рисуем точку 
+                u += 0.25;//прибавляем шаг
+            }
+            Gl.glEnd();
+            
+            AnT.Invalidate();       
+        }
+        private void Form1_Load_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void simpleOpenGlControl1_Load_1(object sender, EventArgs e)
+        {
+
+        }
+   
+        private void InitializeComponent()
+        {
+            this.AnT = new Tao.Platform.Windows.SimpleOpenGlControl();
+            this.SuspendLayout();
+            // 
+            // AnT
+            // 
+            this.AnT.AccumBits = ((byte)(0));
+            this.AnT.AutoCheckErrors = false;
+            this.AnT.AutoFinish = false;
+            this.AnT.AutoMakeCurrent = true;
+            this.AnT.AutoSwapBuffers = true;
+            this.AnT.BackColor = System.Drawing.Color.Black;
+            this.AnT.ColorBits = ((byte)(32));
+            this.AnT.DepthBits = ((byte)(16));
+            this.AnT.Location = new System.Drawing.Point(12, 12);
+            this.AnT.Name = "AnT";
+            this.AnT.Size = new System.Drawing.Size(497, 431);
+            this.AnT.StencilBits = ((byte)(0));
+            this.AnT.TabIndex = 0;
+            // 
+            // Form1
+            // 
+            this.ClientSize = new System.Drawing.Size(560, 455);
+            this.Controls.Add(this.AnT);
+            this.Name = "Form1";
+            this.ResumeLayout(false);
+
+        }
+    }
+}
